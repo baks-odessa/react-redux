5fb7123f2e09ac20035fadcaf9f66347a51f4525
Baks <baks.odessa@gmail.com>
2022-05-22 23:37:47 +0300

    lesson 2, first part, add post form

---
+++ 02_lesson/src/App.js
@@ -0,0 +1,11 @@
+import AddPostForm from './features/posts/AddPostForm';
+
+function App() {
+  return (
+    <main className="App">
+      <AddPostForm />
+    </main>
+  );
+}
+
+export default App;
